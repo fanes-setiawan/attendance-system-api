3d620573fe615e922a98b3173a307c55e2e8e021
fanes-setiawan <fanessetiawa.1401@gmail.com>
2024-11-06 22:11:35 +0700

    f2 : get user by id

--- controllers/UserController.py
+++ controllers/UserController.py
@@ -2,19 +2,46 @@ from flask import jsonify, request
 from models.UserModel import User
 from config import db
 
+
 def get_users():
     users = User.query.all()
     result = [user.to_dict() for user in users]
-    return jsonify({"status": "success", "data": result}), 200
+    response = {
+        "status": "success",
+        "data": result,
+        "message": "Users retrived successfully!",
+    }
+    return (jsonify(response), 200)
+
+
+def get_user(user_id):
+    user = User.query.get(str(user_id))
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    result = user.to_dict()
+    response = {
+        "status": "success",
+        "data": {"user": result},
+        "message": "User retrieved successfuly!",
+    }
+    return (
+        jsonify(response),
+        200,
+    )
+
 
 def add_user():
     data = request.get_json()
     new_user = User(
-        name=data['name'],
-        email=data['email'],
-        phone_number=data.get('phone_number'),
-        password_hash=data['password_hash']
+        name=data["name"],
+        email=data["email"],
+        phone_number=data.get("phone_number"),
+        password_hash=data["password_hash"],
     )
     db.session.add(new_user)
     db.session.commit()
-    return jsonify({"message": "User added successfully"}), 201
+    return (
+        jsonify({"message": "User added successfully", "user": new_user.to_dict()}),
+        201,
+    )
--- routes/User_bp.py
+++ routes/User_bp.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
-from controllers.UserController import get_users, add_user
+from controllers.UserController import get_users, add_user,get_user
 
 user_bp = Blueprint('user_bp', __name__)
 
 user_bp.route('/api/users', methods=['GET'])(get_users)
+user_bp.route('/api/user/<uuid:user_id>', methods=['GET'])(get_user)
 user_bp.route('/api/users', methods=['POST'])(add_user)
