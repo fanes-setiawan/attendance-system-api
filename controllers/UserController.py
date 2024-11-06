704a494d39233af29f0c0a5713f2414e5f8d0d63
fanes-setiawan <fanessetiawa.1401@gmail.com>
2024-11-06 23:07:54 +0700

    f3: update,patch,delete user

--- controllers/UserController.py
+++ controllers/UserController.py
@@ -25,10 +25,7 @@ def get_user(user_id):
         "data": {"user": result},
         "message": "User retrieved successfuly!",
     }
-    return (
-        jsonify(response),
-        200,
-    )
+    return (jsonify(response), 200)
 
 
 def add_user():
@@ -41,7 +38,56 @@ def add_user():
     )
     db.session.add(new_user)
     db.session.commit()
-    return (
-        jsonify({"message": "User added successfully", "user": new_user.to_dict()}),
-        201,
+
+    response = {
+        "message": "User added successfully",
+        "user": new_user.to_dict(),
+    }
+    return (jsonify(response), 201)
+
+
+def update_user(user_id):
+    user = User.query.get(str(user_id))
+    if not user:
+        return jsonify({"error": "user not found"}), 404
+
+    update_data = request.get_json()
+
+    user.name = update_data.get("name", user.name)
+    user.email = update_data.get("email", user.email)
+    user.phone_number = update_data.get("phone_number", user.phone_number)
+    user.password_hash = update_data.get("password_hash", user.password_hash)
+
+    db.session.commit()
+
+    return jsonify({"message": "user updated successfully!", "user": user.to_dict()})
+
+
+def patch_user(user_id):
+    user = User.query.get(str(user_id))
+    if not user:
+        return jsonify({"error": "user not fount"}), 404
+
+    patch_data = request.get_json()
+    if "name" in patch_data:
+        user.name = patch_data["name"]
+    if "email" in patch_data:
+        user.email = patch_data["email"]
+    if "phone_number" in patch_data:
+        user.phone_number = patch_data["phone_number"]
+    if "password_hash" in patch_data:
+        user.password_hash = patch_data["password_hash"]
+
+    db.session.commit()
+    return jsonify(
+        {"message": "user partially updated successfully!", "user": user.to_dict()}
     )
+
+def delete_user(user_id):
+    user = User.query.get(str(user_id))
+    if not user:
+        return jsonify({'error' : 'user not fount'}),404
+    
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message':'user deleted successfully!'})
