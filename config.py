48b01f32a7c9c4b1c142c9796530b7977a6db2f8
fanes <90265405+fanes-setiawan@users.noreply.github.com>
2024-11-11 15:05:04 +0700

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ DB_USERNAME = os.getenv('DB_USERNAME', '')
 DB_PASSWORD = os.getenv('DB_PASSWORD', '')
 DB_HOST = os.getenv('DB_HOST', '')
 DB_NAME = os.getenv('DB_NAME', '')
-DB_NAME = os.getenv('DB_NAME', '3305')
+DB_PORT = os.getenv('DB_PORT', '3305')
 
 # Set the SQLAlchemy Database URI
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
